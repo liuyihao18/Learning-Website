43a279cf606e74ac0720ffc019488d262d6f2ca2
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-07-05 12:58:45 +0800

    complete fundamental index page

--- Learning/views.py
+++ Learning/views.py
@@ -1,6 +1,14 @@
+import platform
+import torch
+
 from django.shortcuts import render
 
 
 # Create your views here.
 def learning_index(request):
-    return render(request, 'index.html', {})
+    context = {
+        'active': 'index',
+        'python_version': platform.python_version(),
+        'pytorch_version': torch.__version__
+    }
+    return render(request, 'Learning/index.html', context)
