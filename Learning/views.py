44952734edebb8efffeb77c6cd4c0c4504e25b6c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-07-05 11:07:36 +0800

    add basic html

--- Learning/views.py
+++ Learning/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def learning_index(request):
-    return render(request, 'Learning/index.html', {})
+    return render(request, 'index.html', {})
