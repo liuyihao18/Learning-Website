ba093b2f39373d23b5e2df5d06a0efa5b826ecad
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-07-06 09:19:39 +0800

    change model

--- Learning/models.py
+++ Learning/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class ModelInfo(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, auto_created=True)
     model = models.CharField(max_length=64)
     optimizer = models.CharField(max_length=64)
     learning_rate = models.FloatField()
@@ -12,5 +12,5 @@ class ModelInfo(models.Model):
     task = models.CharField(max_length=64)
     person = models.CharField(max_length=64)
     begin_time = models.DateTimeField(auto_now_add=True)
-    finish_time = models.DateTimeField(null=True)
+    finish_time = models.DateTimeField(auto_created=True, null=True)
     state = models.CharField(max_length=64)
