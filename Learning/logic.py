0cd21b2791a30f9db70efb45794c99bc1151713d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-07-07 14:51:26 +0800

    change structure

--- Learning/logic.py
+++ Learning/logic.py
@@ -8,28 +8,20 @@ from django.utils import timezone
 from . import constants
 from . import models
 
+tz = datetime.timezone(datetime.timedelta(hours=+8))
 
-def get_task_list_context(page):
-    objs = models.ModelInfo.objects.all().order_by('-id')  # 增加'-'表示逆序
-    max_page = max(math.ceil(objs.count() / constants.page_size), 1)  # 最大页数
-    if page < 1:
-        return {'redirect': '/result/1/'}  # 重定向
-    if page > max_page:
-        return {'redirect': '/result/' + str(max_page) + '/'}  # 重定向
-    left = (page - 1) * constants.page_size  # 左索引
-    right = min(page * constants.page_size, objs.count())  # 右索引
-    objs = objs[left:right]  # 取出部分数据
-    tz = datetime.timezone(datetime.timedelta(hours=+8))
 
-    # 把秒转化为时分秒
-    def change_to_hours_minutes_seconds(seconds: int) -> str:
-        hours = int(seconds / 3600)
-        minutes = int(seconds / 60) % 60
-        seconds = seconds % 60
-        return str(hours).zfill(2) + ':' + str(minutes).zfill(2) + ':' + str(seconds).zfill(2)
+# 把秒转化为时分秒
+def change_to_hours_minutes_seconds(seconds: int) -> str:
+    hours = int(seconds / 3600)
+    minutes = int(seconds / 60) % 60
+    seconds = seconds % 60
+    return str(hours).zfill(2) + ':' + str(minutes).zfill(2) + ':' + str(seconds).zfill(2)
 
-    # 映射字典
-    objs = list(map(lambda obj: {
+
+# 把obj中需要用到的提取出来
+def change(obj):
+    return {
         'id': obj.id,
         'task': obj.task,
         'person': obj.person,
@@ -43,7 +35,22 @@ def get_task_list_context(page):
                        (not obj.finish_time and '-------------------'),
         'raw_state': obj.state,
         'state': constants.translate[obj.state],
-    }, objs))
+    }
+
+
+def get_task_list_context(page):
+    objs = models.ModelInfo.objects.all().order_by('-id')  # 增加'-'表示逆序
+    max_page = max(math.ceil(objs.count() / constants.page_size), 1)  # 最大页数
+    if page < 1:
+        return {'redirect': '/result/1/'}  # 重定向
+    if page > max_page:
+        return {'redirect': '/result/' + str(max_page) + '/'}  # 重定向
+    left = (page - 1) * constants.page_size  # 左索引
+    right = min(page * constants.page_size, objs.count())  # 右索引
+    objs = objs[left:right]  # 取出部分数据
+
+    # 映射字典
+    objs = list(map(change, objs))
     for i in range(len(objs)):
         objs[i]['item'] = left + i + 1
 
@@ -59,8 +66,6 @@ def get_task_list_context(page):
 
     # 内容填充
     context = {
-        'active': 'result',
-        'cols': constants.cols,
         'col_names': constants.col_names,
         'objs': objs,
         'now_page': page,
@@ -74,17 +79,28 @@ def get_task_list_context(page):
 
 
 def get_log_context(page, item):
-    try:
-        log = open(
-            constants.extra_args['save_path'] + os.sep + constants.extra_args['log'] + os.sep + str(item) + ".log", 'r')
-        log_content = log.read()
-        log.close()
-    except FileNotFoundError:
-        log_content = '404 Not Found!'
     context = {
         'return_url': '/result/' + str(page) + '/',
-        'log_content': log_content,
+        'col_names': constants.col_names,
+        'now_page': page,
     }
+    if not models.ModelInfo.objects.filter(id=item):
+        context['not_found'] = True
+    else:
+        # 补充对象信息
+        obj = models.ModelInfo.objects.get(id=item)
+        obj = change(obj)
+        obj['item'] = '*'
+        context['obj'] = obj
+        try:
+            log = open(
+                constants.extra_args['save_path'] + os.sep + constants.extra_args['log'] + os.sep + str(item) + ".log",
+                'r')
+            log_content = log.read()
+            log.close()
+        except FileNotFoundError:
+            log_content = "Sorry, log is missing!"
+        context['log_content'] = log_content
     return context
 
 
@@ -92,11 +108,19 @@ def get_analysis_context(page, item):
     context = {
         'return_url': '/result/' + str(page) + '/',
         'item': item,
+        'col_names': constants.col_names,
+        'now_page': page,
         'loss_curve': constants.extra_args['loss_curve'],
         'accuracy_curve': constants.extra_args['accuracy_curve'],
     }
     if not models.ModelInfo.objects.filter(id=item):
         context['not_found'] = True
+    else:
+        # 补充对象信息
+        obj = models.ModelInfo.objects.get(id=item)
+        obj = change(obj)
+        obj['item'] = '*'
+        context['obj'] = obj
     return context
 
 
