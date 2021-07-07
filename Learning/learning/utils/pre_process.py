171b3ec61211faeb7cfdaf0997fcbb0aceeb86c1
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-07-07 20:11:07 +0800

    use lib

--- Learning/learning/utils/pre_process.py
+++ Learning/learning/utils/pre_process.py
@@ -11,16 +11,9 @@ import torch
 import torchvision
 
 
-class ToTensor(torchvision.transforms.ToTensor):
-    def __call__(self, pic):
-        default_float_dtype = torch.get_default_dtype()
-        img = torchvision.transforms.PILToTensor()(pic)
-        return img.to(dtype=default_float_dtype).div(255)
-
-
 def normal_transform() -> torchvision.transforms.Compose:
     normal = torchvision.transforms.Compose([
-        ToTensor(),  # 转化张量
+        torchvision.transforms.ToTensor(),  # 转化张量
     ])
     return normal
 
@@ -30,6 +23,6 @@ def data_augment_transform() -> torchvision.transforms.Compose:
         torchvision.transforms.RandomCrop(28),  # 随机裁剪
         torchvision.transforms.RandomHorizontalFlip(0.5),  # 水平翻转
         torchvision.transforms.RandomVerticalFlip(0.5),  # 垂直翻转
-        ToTensor(),  # 转化张量
+        torchvision.transforms.ToTensor(),  # 转化张量
     ])
     return data_augment
